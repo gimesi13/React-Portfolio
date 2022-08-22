24098f415a68255f652c7a074bb7a26a451ec3a4
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-08-22 11:40:35 +0300

    Creating Footer section
    constructing the frame of the footer section; minor fixes for responsive styling of the navbar component.

--- react-portfolio-gergely/src/App.js
+++ react-portfolio-gergely/src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home";
 import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ react-portfolio-gergely/src/Components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { BsLinkedin } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsArrowUpCircleFill } from "react-icons/bs";
+
+function Footer() {
+  return (
+    <section className="banner footer-section" id="footer">
+      <motion.div
+        initial={{
+          y: -90,
+        }}
+        className="footer-build-box"
+      >
+        <h1>This Website has been built with:</h1>
+        <div>slider</div>
+      </motion.div>
+
+      <div className="footer-bottom-container">
+        <div className="footer-bottom-left">
+          <BsArrowUpCircleFill className="up-arrow-svg" />
+        </div>
+        <div className="footer-bottom-right">
+          <div className="soc-icons-footer">
+            <div>
+              <BsLinkedin className="social-icons" />
+            </div>
+            <div>
+              <BsFacebook className="social-icons" />
+            </div>
+            <div>
+              <BsGithub className="social-icons" />
+            </div>
+          </div>
+          <div>Created By Gergely Gimesi on 2022.</div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Footer;
