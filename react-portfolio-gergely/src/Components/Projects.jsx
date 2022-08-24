77931dc5f0dc858015611e983e4c4364bee190b4
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-08-24 12:52:51 +0300

    Tab animations Projects section
    Some cleanup in the JSX file needed

--- react-portfolio-gergely/src/Components/Projects.jsx
+++ react-portfolio-gergely/src/Components/Projects.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import thinker from "./Pictures/undraw_code_thinking_re_gka2.svg";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Project from "./Project";
 
 function Projects({ setLocation }) {
   //setting up states for the tab operators
   const [activeTab, setActiveTab] = useState("tab1");
+
+  const tabs = ["tab1", "tab2", "tab3"];
+
+  const animateTab = {
+    inactive: {
+      clipPath: "circle(0% at 50% 50%)",
+      transition: { duration: 0.5 },
+    },
+    active: {
+      clipPath: "circle(70.7% at 50% 50%)",
+      transition: { duration: 0.5 },
+    },
+  };
 
   return (
     <section className="banner projects">
@@ -18,54 +31,68 @@ function Projects({ setLocation }) {
         fun. Check them out:
       </p>
       <div className="tabs">
-        <div
-          onClick={() => {
-            setActiveTab("tab1");
-          }}
-        >
-          tab1
-        </div>
-        <div
-          onClick={() => {
-            setActiveTab("tab2");
-          }}
-        >
-          tab2
-        </div>
-        <div
-          onClick={() => {
-            setActiveTab("tab3");
-          }}
-        >
-          tab3
-        </div>
+        {tabs.map((tab) => {
+          return (
+            <motion.div
+              className="tab"
+              key={tab}
+              initial={false}
+              animate={activeTab === tab ? "active" : "inactive"}
+              onClick={() => {
+                setActiveTab(tab);
+              }}
+            >
+              {tab}
+              <motion.div
+                className="tab-background"
+                variants={animateTab}
+              ></motion.div>
+            </motion.div>
+          );
+        })}
       </div>
-      {activeTab === "tab1" && (
-        <div className="projects-grig tab-1">
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-        </div>
-      )}
+      <AnimatePresence>
+        {activeTab === "tab1" && (
+          <motion.div
+            className="projects-grig tab-1"
+            initial={{ x: "100%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1, display: "grid" }}
+          >
+            <Project />
+            <Project />
+            <Project />
+            <Project />
+            <Project />
+            <Project />
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-      {activeTab === "tab2" && (
-        <div className="projects-grig tab-2">
-          <div className="thinker">
-            <img className="thinker-svg" src={thinker} alt="thinker" />
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A
-            voluptatibus blanditiis nihil non, iste ipsam perferendis debitis
-            nulla! Quam, autem.
-          </p>
-        </div>
-      )}
+      <AnimatePresence>
+        {activeTab === "tab2" && (
+          <motion.div
+            className="projects-grig tab-2"
+            initial={{ x: "100%", opacity: 0 }}
+            animate={{ x: 0, opacity: 1, display: "grid" }}
+          >
+            <div className="thinker">
+              <img className="thinker-svg" src={thinker} alt="thinker" />
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+              voluptatibus blanditiis nihil non, iste ipsam perferendis debitis
+              nulla! Quam, autem.
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       {activeTab === "tab3" && (
-        <div className="projects-grig tab-3">
+        <motion.div
+          className="projects-grig tab-3"
+          initial={{ x: "100%", opacity: 0 }}
+          animate={{ x: 0, opacity: 1, display: "grid" }}
+        >
           <div className="thinker">
             <img className="thinker-svg" src={thinker} alt="thinker" />
           </div>
@@ -73,7 +100,7 @@ function Projects({ setLocation }) {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
             veritatis.
           </p>
-        </div>
+        </motion.div>
       )}
     </section>
   );
